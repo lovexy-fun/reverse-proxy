932045f4b4e2b3d8a4ba6b3bad003042a25d42b2
lovexy-fun <lovexyfun@qq.com>
2021-06-09 22:03:10 +0800

    style:增加一位版本号

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 @echo off
-start javaw -Xms300m -Xmx300m -XX:CompressedClassSpaceSize=256M -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -jar reverse-proxy-1.0-SNAPSHOT.jar --spring.config.location=application.yml --basepath=%~dp0
+start javaw -Xms300m -Xmx300m -XX:CompressedClassSpaceSize=256M -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -jar reverse-proxy-1.0.0-SNAPSHOT.jar --spring.config.location=application.yml --basepath=%~dp0
